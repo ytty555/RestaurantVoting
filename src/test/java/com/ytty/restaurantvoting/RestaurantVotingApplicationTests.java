5055508e6009e998a6ef366119e93e183601c581
Yuriy Titov <yurtitov@gmail.com>
2022-11-01 23:13:25 +0200

    first commit

---
+++ src/test/java/com/ytty/restaurantvoting/RestaurantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytty.restaurantvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
